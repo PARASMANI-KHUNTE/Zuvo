dfb6405d91b411665f5cb2a08028ca7d40c2e226
PARASMANI-KHUNTE <parasmanikhunte@gmail.com>
2026-03-01 14:29:37 +0530

    chore: Remove file.

--- backend/check_exports.js
+++
@@ -1,3 +0,0 @@
-const auth = require("./services/auth/src/controllers/auth");
-console.log("Exports from auth controller:");
-Object.keys(auth).forEach(k => console.log(`- ${k}: ${typeof auth[k]}`));
